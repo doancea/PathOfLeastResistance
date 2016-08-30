c0949b5a393cb036cfa5a78370fa245421e42d10
Daniel Oancea <doancea@pillartechnology.com>
2016-08-30 15:28:35 -0400

    Updated Integration test to fail when requirements are met to find the actual minimal path

--- app/src/test/java/com/doancea/pathofleastresistance/Grid/TraversalDelegateIntegrationTests.java
+++ app/src/test/java/com/doancea/pathofleastresistance/Grid/TraversalDelegateIntegrationTests.java
@@ -21,10 +21,10 @@ public class TraversalDelegateIntegrationTests {
     public void findThePathOfImmediateLeastResistance() {
         int[][] gridValues = {
                 {9, 0, 8, 1, 7, 2, 6, 3, 5, 4},
-                {4, 5, 3, 6, 2, 7, 1, 8, 0, 9},
-                {1, 2, 3, 4, 5, 6, 7, 8, 9, 0},
-                {0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
-                {9, 8, 7, 6, 5, 4, 3, 2, 1, 0}
+                {8, 9, 7, 6, 2, 3, 1, 4, 0, 5},
+                {1, 7, 8, 9, 5, 6, 2, 3, 4, 0},
+                {7, 8, 9, 3, 4, 5, 6, 0, 1, 2},
+                {9, 8, 0, 6, 5, 4, 3, 2, 1, 7}
         };
 
         grid = new ResistanceGrid(gridValues);
