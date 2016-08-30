9714b6fc225e839802093e61c022aa152b77f045
Daniel Oancea <doancea@pillartechnology.com>
2016-08-30 14:42:38 -0400

    Implemented intial walk across a grid to add all points in the path

--- app/src/main/java/com/doancea/pathofleastresistance/Grid/TraversalDelegate.java
+++ app/src/main/java/com/doancea/pathofleastresistance/Grid/TraversalDelegate.java
@@ -3,14 +3,29 @@ package com.doancea.pathofleastresistance.Grid;
 
 import com.doancea.pathofleastresistance.Path.TraversalResult;
 
+import java.lang.ref.WeakReference;
+
 public class TraversalDelegate {
 
+    private GridTraverser traverser;
+    private ResistanceGrid grid;
+    private TraversalResult result;
 
-    public TraversalDelegate(GridTraverser traverser) {
-
+    public TraversalDelegate(GridTraverser traverser, ResistanceGrid grid) {
+        this.traverser = traverser;
+        this.grid = grid;
+        this.result = new TraversalResult();
     }
 
     public TraversalResult getPathOfLeastResistance() {
-        return null;
+        GridPoint startPoint = new GridPoint();
+        WeakReference<GridPoint> currentPoint = new WeakReference(startPoint);
+
+        while(result.getPath().size() < grid.getNumberOfColumns()) {
+            currentPoint = new WeakReference(traverser.getNextPosition(currentPoint.get(), this.grid));
+            result.addPointToPath(currentPoint.get());
+        }
+
+        return result;
     }
 }
---
+++ app/src/test/java/com/doancea/pathofleastresistance/Grid/TraversalDelegateTest.java
@@ -0,0 +1,65 @@
+package com.doancea.pathofleastresistance.Grid;
+
+import com.doancea.pathofleastresistance.Path.TraversalResult;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
+
+public class TraversalDelegateTest {
+
+    private GridTraverser traverser;
+    private ResistanceGrid grid;
+    private TraversalDelegate subject;
+
+    @Before
+    public void setup() {
+        traverser = Mockito.mock(GridTraverser.class);
+        grid = Mockito.mock(ResistanceGrid.class);
+
+        subject = new TraversalDelegate(traverser, grid);
+    }
+
+    @Test
+    public void getPathOfLeastResistanceReturnsATraversalResultWithContainingAllOfThePointsForTheResultantPath() {
+        int numberOfColumns = 5;
+        when(grid.getNumberOfColumns()).thenReturn(numberOfColumns);
+        GridPositionAnswer positionAnswer = new GridPositionAnswer();
+
+        when(traverser.getNextPosition(any(GridPoint.class), any(ResistanceGrid.class))).thenAnswer(positionAnswer);
+
+        TraversalResult result = subject.getPathOfLeastResistance();
+        List<GridPoint> actualPath = result.getPath();
+
+        assertThat(actualPath.size(), is(numberOfColumns));
+        assertThat(actualPath, is(positionAnswer.getExpectedPath()));
+    }
+
+
+    private class GridPositionAnswer implements Answer<GridPoint> {
+        int currentColumn = 0;
+        List<GridPoint> expectedPath = new ArrayList<>();
+
+        @Override
+        public GridPoint answer(InvocationOnMock invocation) throws Throwable {
+            GridPoint newPoint = new GridPoint(currentColumn++, 1);
+            expectedPath.add(newPoint);
+            return newPoint;
+        }
+
+        public List<GridPoint> getExpectedPath() {
+            return this.expectedPath;
+        }
+
+    }
+}
